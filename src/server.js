d4c4bc1e4cac3ca44f6515bc851b6c77b061284f
Vladyslav6 <137646785+Vladyslav6@users.noreply.github.com>
2025-06-13 14:40:25 +0300

    Add!ContactNF

--- src/server.js
+++ src/server.js
@@ -51,13 +51,13 @@ export const setupServer = () => {
   app.get('/contacts/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
     const contact = await Contact.findById(contactId);
-    //
-    // if (!contact) {
-    //   return res.status(404).json({
-    //     status: 404,
-    //     message: `Contact with id ${contactId}, note found`,
-    //   });
-    // }
+    
+    if (!contact) {
+      return res.status(404).json({
+        status: 404,
+        message: `Contact not found`,
+      });
+    }
     res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
