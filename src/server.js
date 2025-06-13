cae2b2437915962d72e1d7b59ae0c254144444d0
Vladyslav6 <137646785+Vladyslav6@users.noreply.github.com>
2025-06-13 14:45:10 +0300

    addTestFix

--- src/server.js
+++ src/server.js
@@ -55,7 +55,7 @@ export const setupServer = () => {
     if (!contact) {
       return res.status(404).json({
         status: 404,
-        message: `Contact not found`,
+        message: `Contact not foundTest`,
       });
     }
     res.status(200).json({
