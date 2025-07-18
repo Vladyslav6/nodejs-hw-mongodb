576c69a94b538c84baa43040d12406358403a872
Vladyslav6 <vlad51284@gmail.com>
2025-07-19 01:48:29 +0300

    addSwagger

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 //
 //
 //
@@ -41,6 +42,9 @@ export const setupServer = () => {
   //
   app.use('/uploads', express.static(UPLOAD_DIR));
   //
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(errorHandler);
 
   app.use(notFoundHandler);
